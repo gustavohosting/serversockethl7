944cf45bee95f15f2fbdef511dffd21b207f0521
Gustavo Heredia <gustavoarielheredia@gmail.com>
2023-07-13 09:03:21 -0300

    se borraron comentarios

--- ServerSocketHL7/Program.cs
+++ ServerSocketHL7/Program.cs
@@ -153,17 +153,6 @@ namespace MultiThreadedTcpEchoServer
                             Console.WriteLine(mensajeEspacio + "Se envio ACK resultado Ok-->");
                         }
                     }
-                    //else if (hl7Data.Length == 1 && hl7Data.IndexOf(EOT) == 0 && recibiendoResultados)
-                    //{
-                    //    //-----------------------------------------------------------------
-                    //    //conexion aceptada y establecida por el equipo!!
-                    //    //-----------------------------------------------------------------
-                    //    conexion = true;// conexion acepta
-                    //    mensajeResultados += mensajeResultados + hl7Data;
-                    //    hl7Data = String.Empty;
-                    //    Console.WriteLine(mensajeEspacio + "-->EOT del equipo finalizo envio de resultados!!");
-                    //    recibiendoResultados = false;
-                    //}
                     else {
                         mensajeResultados += mensajeResultados + hl7Data;
                         hl7Data = String.Empty;//limpio mensaje
@@ -184,57 +173,12 @@ namespace MultiThreadedTcpEchoServer
                 tcpClientConnection.Close();
             }
         }
-        private string GetSimpleAcknowledgementMessage(string incomingHl7Message)
-        {
-            //if (string.IsNullOrEmpty(incomingHl7Message))
-            //    throw new ApplicationException("Invalid HL7 message for parsing operation. Please check your inputs");
-
-            ////retrieve the message control ID of the incoming HL7 message
-            //var messageControlId = GetMessageControlID(incomingHl7Message);
-
-            ////build an acknowledgement message and include the control ID with it
-            //var ackMessage = new StringBuilder();
-            //ackMessage = ackMessage.Append(START_OF_BLOCK)
-            //    .Append("MSH|^~\\&|||||||ACK||P|2.2")
-            //    .Append(CARRIAGE_RETURN)
-            //    .Append("MSA|AA|")
-            //    .Append(messageControlId)
-            //    .Append(CARRIAGE_RETURN)
-            //    .Append(END_OF_BLOCK)
-            //    .Append(CARRIAGE_RETURN);
-
-            //return ackMessage.ToString();
-            return string.Empty;
-        }
 
         private string GetAckMessage()
         {
             var ackMessage = new StringBuilder();
             ackMessage = ackMessage.Append(ACK);
             return ackMessage.ToString();
-        }
-
-        private string GetMessageControlID(string incomingHl7Message)
-        {
-
-            //var fieldCount = 0;
-            ////parse the message into segments using the end of segment separter
-            //var hl7MessageSegments = incomingHl7Message.Split(CARRIAGE_RETURN);
-
-            ////tokenize the MSH segment into fields using the field separator
-            //var hl7FieldsInMshSegment = hl7MessageSegments[0].Split(FIELD_DELIMITER);
-
-            ////retrieve the message control ID in order to reply back with the message ack
-            //foreach (var field in hl7FieldsInMshSegment)
-            //{
-            //    if (fieldCount == MESSAGE_CONTROL_ID_LOCATION)
-            //    {
-            //        return field;
-            //    }
-            //    fieldCount++;
-            //}
-
-            return string.Empty; //you can also throw an exception here if you wish
         }
     }
 }
